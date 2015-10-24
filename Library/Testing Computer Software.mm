02a03c2924965a4b4c6b8674e15386952c7cc937
Anil Sakala <anil.sakala@gmail.com>
2015-10-24 16:41:56 +0530

    new testing tools

---
+++ Library/Testing Computer Software.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1445679504647" ID="ID_959886658" MODIFIED="1445679524054" TEXT="Testing Computer Software">
+<node CREATED="1445680806359" ID="ID_1899688122" MODIFIED="1445681024739" POSITION="right" TEXT="1. An example test series">
+<node CREATED="1445680821998" ID="ID_605575846" MODIFIED="1445680827995" TEXT="First cycle of testing">
+<node CREATED="1445680838712" ID="ID_277338678" MODIFIED="1445680851195" TEXT="Have basic understanding of program"/>
+<node CREATED="1445680853312" ID="ID_490742831" MODIFIED="1445680891020" TEXT="Simple tests - Focus on on screen , error , output"/>
+<node CREATED="1445680924335" ID="ID_72549410" MODIFIED="1445680939212" TEXT="Boundary testing is important"/>
+<node CREATED="1445680962046" ID="ID_1708597718" MODIFIED="1445681777146" TEXT="Summarize about the program and its problems . This is just for you and you can pass to your team member"/>
+</node>
+<node CREATED="1445681026041" ID="ID_1513696487" MODIFIED="1445681033075" TEXT="Second cycle of testing">
+<node CREATED="1445681779637" ID="ID_1407692760" MODIFIED="1445681797234" TEXT="Go thorugh the comments and add more test cases"/>
+<node CREATED="1445681799436" ID="ID_1006311233" MODIFIED="1445681806066" TEXT="Strat basic testing"/>
+<node CREATED="1445681809661" ID="ID_1152939290" MODIFIED="1445681825922" TEXT="Add more test cases and complete testing"/>
+</node>
+<node CREATED="1445681925397" ID="ID_656250993" MODIFIED="1445681934346" TEXT="Later cycles of testing">
+<node CREATED="1445682086286" ID="ID_1458401712" MODIFIED="1445682092570" TEXT="Create more formal test series"/>
+</node>
+</node>
+<node CREATED="1445683493010" ID="ID_1839858156" MODIFIED="1445683504591" POSITION="left" TEXT="2.Objectives and limits of testing">
+<node CREATED="1445684743112" ID="ID_1866855435" MODIFIED="1445684794287" TEXT="You cannot test a program completely. You will have 1 millon good and bad combinations even for a simple program like find sum of two numbers"/>
+<node CREATED="1445684980008" ID="ID_926202216" MODIFIED="1445685008692" TEXT="The purpose of testing is to find a problem in it . The prime benefit of testing is that it results in improved quality. Bugs get fixed"/>
+</node>
+</node>
+</map>
--- Library/_Books.mm
+++ Library/_Books.mm
@@ -1,4 +1,8 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1445526397919" ID="ID_1578984446" MODIFIED="1445526410215" TEXT="Books"/>
+<node CREATED="1445526397919" ID="ID_1578984446" MODIFIED="1445526410215" TEXT="Books">
+<node CREATED="1445679487150" ID="ID_1468694612" MODIFIED="1445679490863" POSITION="right" TEXT="Testing">
+<node CREATED="1445679495193" ID="ID_661062076" LINK="Testing%20Computer%20Software.mm" MODIFIED="1445685046069" TEXT="Testing computer software"/>
+</node>
+</node>
 </map>
