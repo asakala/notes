aa218cda5b7ff33b5945ed272ecaf6af871b494e
Anil Sakala <anil.sakala@gmail.com>
2015-10-26 10:33:06 +0530

    many changes

---
+++ Library/Soft Skills The software developers manual.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1445780971542" ID="ID_1874919614" MODIFIED="1445783512051" TEXT="Soft Skills: The software developers manual">
+<node CREATED="1445780983193" ID="ID_1448413491" MODIFIED="1445780986099" POSITION="right" TEXT="Career">
+<node CREATED="1445789554505" ID="ID_1141938720" MODIFIED="1445789581159" TEXT="Think like a businessman"/>
+<node CREATED="1445789591569" ID="ID_1946552317" MODIFIED="1445789597749" TEXT="Have life goal "/>
+<node CREATED="1445789638313" ID="ID_1077585251" MODIFIED="1445789644199" TEXT="People skills"/>
+<node CREATED="1445791318183" ID="ID_46858446" MODIFIED="1445791324277" TEXT="Hacking the interview"/>
+</node>
+<node CREATED="1445789633778" ID="ID_779554028" MODIFIED="1445789633778" POSITION="left" TEXT=""/>
+</node>
+</map>
