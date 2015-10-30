26a4abd077e1b5a024b5a4c29412c2ad3fc7282b
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-10-30 09:53:33 -0500

    dssd

---
+++ Library/Five Prokarma Presentations.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1446215843480" ID="ID_1644295484" MODIFIED="1446215858287" TEXT="Five Prokarma Presentations">
+<node CREATED="1446215861288" ID="ID_599323507" MODIFIED="1446215865526" POSITION="right" TEXT="Clean Code"/>
+<node CREATED="1446215867879" ID="ID_1999360438" MODIFIED="1446215884072" POSITION="left" TEXT="Logging Practices"/>
+<node CREATED="1446215886607" ID="ID_125219509" MODIFIED="1446215923545" POSITION="right" TEXT="Scalable Architecture"/>
+<node CREATED="1446215925888" ID="ID_1832295778" MODIFIED="1446215929624" POSITION="left" TEXT="Kaizen"/>
+<node CREATED="1446215931793" ID="ID_1565551913" MODIFIED="1446215940641" POSITION="right" TEXT="Engaged Employess">
+<node CREATED="1446216016429" ID="ID_1233769136" MODIFIED="1446216237653" TEXT="Who are engaged employees"/>
+<node CREATED="1446216240229" ID="ID_1732248771" MODIFIED="1446216261480" TEXT="How engaged employees increase customer satisfaction and bring down attrition rate"/>
+<node CREATED="1446216275893" ID="ID_88010910" MODIFIED="1446216284304" TEXT="Where do we get engaged employees"/>
+<node CREATED="1446216327462" ID="ID_284532145" MODIFIED="1446216357689" TEXT="What is &quot;RESPECT&quot; model to create engaged employees"/>
+</node>
+</node>
+</map>
---
+++ Library/Kaizen.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1446210837094" ID="ID_1332106773" MODIFIED="1446210949975" TEXT="Kaizen">
+<node CREATED="1446210955001" ID="ID_1113747180" MODIFIED="1446210970993" POSITION="right" TEXT="What is Kaizen">
+<node CREATED="1446210974623" ID="ID_1657963217" MODIFIED="1446211075359" TEXT="Every one wants to show some improvement in their personal life , professional life">
+<node CREATED="1446211189938" ID="ID_1415253024" MODIFIED="1446211260621" TEXT="personal life = reduce weight , more happy , improve your health"/>
+<node CREATED="1446211273628" ID="ID_1961210165" MODIFIED="1446211331720" TEXT="professional life = improve your communication skills , improve your leadre ship skills, improve your technical skills , improve your productivity"/>
+</node>
+<node CREATED="1446211445441" ID="ID_1992019976" MODIFIED="1446211494502" TEXT="We find it very difficult to show some improvement . Why ?">
+<node CREATED="1446211519341" ID="ID_130123177" MODIFIED="1446211534050" TEXT="For example you want to reduce wait">
+<node CREATED="1446211536396" ID="ID_1879120150" MODIFIED="1446211544338" TEXT="conventional wisdom fails"/>
+</node>
+</node>
+<node CREATED="1446213830671" ID="ID_1717194554" MODIFIED="1446213848455" TEXT="Kaizen is a new methodology to show improvement"/>
+<node CREATED="1446213859023" ID="ID_1657373380" MODIFIED="1446213926728" TEXT="Kaizen = Problem + Apply Kaizen = Improvement"/>
+</node>
+</node>
+</map>
