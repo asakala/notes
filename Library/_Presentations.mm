4c59238f575cf2fc1dacf734fb77c7d66411cf74
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-11-04 09:10:13 -0600

    dfsdfsdfs

---
+++ Library/_Presentations.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1446215843480" ID="ID_1644295484" MODIFIED="1446637039237" TEXT="Presentations">
+<node CREATED="1446215861288" HGAP="14" ID="ID_599323507" LINK="Clean%20Code.pdf" MODIFIED="1446639823785" POSITION="right" TEXT="Clean Code" VSHIFT="-51"/>
+<node CREATED="1446215867879" HGAP="8" ID="ID_1999360438" MODIFIED="1446639867028" POSITION="left" TEXT="Logging Practices" VSHIFT="-70"/>
+<node CREATED="1446215886607" HGAP="36" ID="ID_125219509" LINK="Scalability.pdf" MODIFIED="1446639826687" POSITION="right" TEXT="Scalable Architecture" VSHIFT="-9"/>
+<node CREATED="1446215925888" HGAP="11" ID="ID_1832295778" MODIFIED="1446640074584" POSITION="left" TEXT="Kaizen" VSHIFT="56"/>
+<node CREATED="1446215931793" HGAP="15" ID="ID_1565551913" LINK="Engaged%20Employees.pdf" MODIFIED="1446641147744" POSITION="right" TEXT="Engaged Employess" VSHIFT="33"/>
+</node>
+</map>
