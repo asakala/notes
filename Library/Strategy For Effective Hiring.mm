cb9a5d94b8e09811b99f8e588f4ed1a8c5e02c0c
Anil Sakala <anil.sakala@gmail.com>
2015-11-07 17:24:21 +0530

    Dfdfgdf

---
+++ Library/Strategy For Effective Hiring.mm
@@ -0,0 +1,112 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1446830963126" ID="ID_1495392880" MODIFIED="1446886033687" TEXT="Strategy For Effective Hiring">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1446834686110" FOLDED="true" HGAP="11" ID="ID_1612737896" MODIFIED="1446897227672" POSITION="right" TEXT="Initial screening  - This is where our bad hiring starts" VSHIFT="-67">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1446836618877" HGAP="34" ID="ID_165534292" MODIFIED="1446889266011" TEXT="Get right set of resumes" VSHIFT="3">
+<node CREATED="1446835763602" ID="ID_142695545" LINK="ProkarmaOnsite.docx" MODIFIED="1446892060297" TEXT="TA team should promote brand the way our onsite group is doing">
+<node CREATED="1446835813636" ID="ID_1085697200" MODIFIED="1446835828657" TEXT="Add more people to your network"/>
+<node CREATED="1446835830900" ID="ID_1593455334" LINK="Content%20that%20can%20be%20posted.docx" MODIFIED="1446894075150" TEXT="Identify few posters and contents that need to be shared again and again ."/>
+<node CREATED="1446836235320" ID="ID_764398302" MODIFIED="1446892143144" TEXT="Play an active role in linkedin by like , share and  comment anything"/>
+</node>
+<node CREATED="1446835867572" ID="ID_682654078" MODIFIED="1446835986475" TEXT="Prokarma  managers should  promote brand">
+<node CREATED="1446894094353" ID="ID_442903533" MODIFIED="1446894688621" TEXT="Effective linked in profile and play the same role recruiters are playing"/>
+<node CREATED="1446894116130" ID="ID_1788508942" LINK="https://twhyderabad.github.io/geeknight/" MODIFIED="1446894670671" STYLE="fork" TEXT="Create a platform or be a part of platform where we can promote ourselves .Our onsite guys are doing it."/>
+</node>
+<node CREATED="1446835885884" ID="ID_1988274616" MODIFIED="1446836221157" TEXT="Partner employees to promote brand"/>
+<node CREATED="1446834718542" ID="ID_571442724" MODIFIED="1446891045433" TEXT="Passive employees need to be targeted- www.makemechange.in"/>
+<node CREATED="1446834732383" ID="ID_1138878810" MODIFIED="1446834741612" TEXT="Referrals need to be encouraged"/>
+</node>
+<node CREATED="1446836664605" HGAP="83" ID="ID_881946504" MODIFIED="1446894698688" TEXT="Short list  from the right set of resumes" VSHIFT="-73">
+<node CREATED="1446840974546" HGAP="19" ID="ID_754387022" MODIFIED="1446873916350" TEXT="KPIs for quality hire" VSHIFT="2">
+<node CREATED="1446889497717" ID="ID_1835156345" MODIFIED="1446889538121" TEXT="Employee retention is top priority (Source : LinkedIn research)"/>
+<node CREATED="1446889593157" ID="ID_1630476966" MODIFIED="1446889703097" TEXT="New hire performance evaluation"/>
+<node CREATED="1446889704989" ID="ID_1753447219" MODIFIED="1446889861906" TEXT="Time taken to fill"/>
+</node>
+<node CREATED="1446873768972" HGAP="35" ID="ID_517131893" MODIFIED="1446894740480" TEXT="Experience &gt;  5" VSHIFT="-14">
+<node CREATED="1446885383968" ID="ID_65197126" MODIFIED="1446885402221" TEXT="Validate profile with linkedin profile">
+<node CREATED="1446894868393" ID="ID_1345560586" LINK="https://in.linkedin.com/in/sumanprofile" MODIFIED="1446895571899" TEXT="suman"/>
+<node CREATED="1446894918849" ID="ID_986394986" LINK="https://in.linkedin.com/pub/nadeem-mohammad/17/411/21" MODIFIED="1446895097311" TEXT="nadeem"/>
+<node CREATED="1446895099673" ID="ID_630265127" LINK="https://www.linkedin.com/pub/rushikesh-samaiahgare/ba/97a/296" MODIFIED="1446895562637" TEXT="rishi"/>
+<node CREATED="1446895262761" ID="ID_1206015228" LINK="https://www.linkedin.com/pub/yellappa-sakaray/14/386/58" MODIFIED="1446895552979" TEXT="yel"/>
+</node>
+<node CREATED="1446885404887" ID="ID_1549214251" MODIFIED="1446885509134" TEXT="All profiles that suite requirements can be passed to any person in second round panel . They will have look at profiles to shortlist profile"/>
+</node>
+<node CREATED="1446873782188" ID="ID_1425035985" MODIFIED="1446886013064" TEXT="Experience &lt; 5">
+<node CREATED="1446885602237" ID="ID_502620204" MODIFIED="1446885761992" TEXT="Employees with less than 5 years may not have account in linkedIn"/>
+<node CREATED="1446885638260" ID="ID_1820379291" MODIFIED="1446894728310" TEXT="Have technical screening interview with 4 or 5 solid questions on core java"/>
+</node>
+</node>
+<node CREATED="1446879145546" HGAP="38" ID="ID_821491118" MODIFIED="1446889255257" TEXT="Check points to know about our progress in this area" VSHIFT="3">
+<node CREATED="1446884331101" ID="ID_727163974" MODIFIED="1446884375706" TEXT="When we login to linkedin I should get same feeling that I get about our prokarma onsite"/>
+<node CREATED="1446884377620" ID="ID_47106320" MODIFIED="1446884845934" TEXT="Success ratio between number of interviews conducted and number of people who got selected in interview"/>
+</node>
+</node>
+<node CREATED="1446836896832" FOLDED="true" HGAP="89" ID="ID_876308215" MODIFIED="1446897225666" POSITION="right" TEXT="Organisation evaluates candidate" VSHIFT="-11">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1446836948441" HGAP="23" ID="ID_1047422546" MODIFIED="1446841147619" TEXT="First round" VSHIFT="-108">
+<node CREATED="1446837087114" HGAP="30" ID="ID_651794033" MODIFIED="1446841165987" TEXT="Interview room" VSHIFT="-3">
+<node CREATED="1446837239932" HGAP="19" ID="ID_535158139" MODIFIED="1446884996596" TEXT="Problems" VSHIFT="-24">
+<node CREATED="1446837201683" ID="ID_508696533" MODIFIED="1446837211855" TEXT="Chairs are broken"/>
+<node CREATED="1446837213763" ID="ID_1117647279" MODIFIED="1446837222889" TEXT="Boards are not clean"/>
+<node CREATED="1446837224819" ID="ID_385864895" MODIFIED="1446837346195" TEXT="Table starts shaking when he starts writing code"/>
+<node CREATED="1446837268948" ID="ID_1597150444" MODIFIED="1446837279129" TEXT="Wires are spread every where"/>
+</node>
+<node CREATED="1446837259292" HGAP="17" ID="ID_513926364" MODIFIED="1446885000364" TEXT="How they should be" VSHIFT="16">
+<node CREATED="1446837360126" ID="ID_1141666789" MODIFIED="1446837393524" TEXT="Comfortable chairs and tables"/>
+<node CREATED="1446837396374" ID="ID_1441467608" MODIFIED="1446837412427" TEXT="Room should talk about what we do"/>
+<node CREATED="1446837435878" ID="ID_1898160524" MODIFIED="1446837463684" TEXT="Candidate should feel he is joining first class company with first class employees"/>
+</node>
+</node>
+<node CREATED="1446837096714" HGAP="28" ID="ID_1310863048" MODIFIED="1446873389645" TEXT="Interviewing panel" VSHIFT="27">
+<node CREATED="1446837117634" ID="ID_1036064027" MODIFIED="1446838631947" TEXT="Competency - Asking simple questions and not knowing what to ask" VSHIFT="-42">
+<node CREATED="1446838073461" ID="ID_1870667237" MODIFIED="1446838149220" TEXT="Train employees"/>
+<node CREATED="1446838120830" HGAP="23" ID="ID_1576095244" MODIFIED="1446871613324" TEXT="Have monthly meetings with the panel " VSHIFT="37">
+<node CREATED="1446838503547" ID="ID_1550629023" MODIFIED="1446838518047" TEXT="Great leaders ask great questions"/>
+<node CREATED="1446838520203" ID="ID_469605184" MODIFIED="1446838526127" TEXT="Connects people"/>
+<node CREATED="1446838528275" ID="ID_1751720761" MODIFIED="1446838533311" TEXT="Opens doors"/>
+<node CREATED="1446838539003" ID="ID_1994922383" MODIFIED="1446838548727" TEXT="Helps us stay focused"/>
+<node CREATED="1446871597957" ID="ID_306048962" MODIFIED="1446871603754" TEXT="Interview experiences"/>
+</node>
+</node>
+<node CREATED="1446837135227" ID="ID_1896148114" MODIFIED="1446837144800" TEXT="Availability">
+<node CREATED="1446838555755" ID="ID_1434022603" MODIFIED="1446838584728" TEXT="Availability can be improved by increasing competency"/>
+<node CREATED="1446838588675" ID="ID_1150275367" MODIFIED="1446838623840" TEXT="Appreciating employees for interview can also increase availability"/>
+</node>
+</node>
+</node>
+<node CREATED="1446836968617" HGAP="26" ID="ID_666342290" MODIFIED="1446869888888" TEXT="Second round" VSHIFT="-46">
+<node CREATED="1446841438701" ID="ID_1648934855" MODIFIED="1446841445098" TEXT="Availability"/>
+<node CREATED="1446841447357" HGAP="21" ID="ID_1437837213" MODIFIED="1446873695146" TEXT="Inconsistency" VSHIFT="17">
+<node CREATED="1446841671352" ID="ID_1591814696" MODIFIED="1446846220109" TEXT="Some managers don&apos;t have expectations"/>
+<node CREATED="1446841684457" ID="ID_927811666" MODIFIED="1446846252693" TEXT="Some managers have expectations which are not known to anyone"/>
+<node CREATED="1446872672935" ID="ID_812110958" MODIFIED="1446872690382" TEXT="Long run strategies are highly considered"/>
+</node>
+</node>
+</node>
+<node CREATED="1446836903376" FOLDED="true" HGAP="23" ID="ID_885760434" MODIFIED="1446897228952" POSITION="right" TEXT="Candidate evaluates organization" VSHIFT="67">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1446837644257" HGAP="42" ID="ID_617976210" MODIFIED="1446847056737" TEXT="Glassdoor" VSHIFT="-13">
+<node CREATED="1446846395177" ID="ID_695075949" MODIFIED="1446846408429" TEXT="Summary of glassdoor comments">
+<node CREATED="1446846823081" ID="ID_1402808363" MODIFIED="1446846834238" TEXT="Overall rating for HYD is 3.0"/>
+<node CREATED="1446846475721" ID="ID_1409311658" MODIFIED="1446846484733" TEXT="Good technoligies"/>
+<node CREATED="1446846486665" ID="ID_157074382" MODIFIED="1446846503750" TEXT="Bad Managers &amp; HR"/>
+<node CREATED="1446846505505" ID="ID_1490090202" MODIFIED="1446846578188" TEXT="Not even a single comment saying salary is less . In fact they say pay is more and don&apos;t get attracted by it "/>
+</node>
+<node CREATED="1446846739641" ID="ID_361141599" MODIFIED="1446847373542" TEXT="How to overcome glassdoor comments">
+<node CREATED="1446846322784" ID="ID_316241914" MODIFIED="1446871561182" TEXT="Invite people for discussion and explain what we are doing and what we are expecting .Be open to all questions" VSHIFT="-16"/>
+<node CREATED="1446846366040" ID="ID_725024682" LINK="Engaged%20Employees.pdf" MODIFIED="1446871548239" TEXT="Educate managers as it is managers issue" VSHIFT="7"/>
+</node>
+</node>
+<node CREATED="1446837650801" HGAP="52" ID="ID_1701775743" MODIFIED="1446846998969" TEXT="Friends" VSHIFT="-2"/>
+<node CREATED="1446837655569" HGAP="42" ID="ID_693336437" MODIFIED="1446846993009" TEXT="Interview experience" VSHIFT="9">
+<node CREATED="1446847418387" HGAP="23" ID="ID_482205573" MODIFIED="1446871567207" TEXT="This should be discussed consistently and improved in monthly meeting with interview panel" VSHIFT="2"/>
+</node>
+<node CREATED="1446870479248" HGAP="41" ID="ID_672506766" MODIFIED="1446871504970" TEXT="Join for 1 week and quit" VSHIFT="16">
+<node CREATED="1446870492066" ID="ID_412933831" MODIFIED="1446871499480" TEXT="Mapping him to  manager who needs to make him more connected to work os required" VSHIFT="2"/>
+</node>
+<node CREATED="1446873991392" HGAP="35" ID="ID_1600360243" MODIFIED="1446874002471" TEXT="Salary" VSHIFT="10"/>
+</node>
+</node>
+</map>
