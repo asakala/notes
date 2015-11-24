0c5668cb268deeabc297af8b6a284705a165b455
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-11-24 08:09:20 -0600

    fsdfsfsdf

---
+++ Library/Core  & Iris meeting.mm
@@ -0,0 +1,22 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447855185886" ID="ID_744215655" MODIFIED="1448346441656" TEXT="Tomorrow Meeting">
+<node CREATED="1447999418467" ID="ID_951425912" MODIFIED="1448004172517" POSITION="right" TEXT="Good Things ">
+<node CREATED="1447999507810" ID="ID_784329232" MODIFIED="1448028082897" TEXT="Excellent participation in code reviews and we should learn from code reviews"/>
+<node CREATED="1447999724510" ID="ID_517237908" MODIFIED="1447999739603" TEXT="Firoz and Goutam helped in assessing question paper"/>
+<node CREATED="1448004172518" ID="ID_939328844" MODIFIED="1448004176154" TEXT="Prod alerts">
+<node CREATED="1448004177610" ID="ID_1316655348" MODIFIED="1448004184578" TEXT="Viswanath Mitta , "/>
+</node>
+</node>
+<node CREATED="1447999427597" ID="ID_610997063" MODIFIED="1447999440489" POSITION="left" TEXT="Topics for discussion">
+<node CREATED="1447999529577" ID="ID_203588763" MODIFIED="1447999590696" TEXT="Important people assets">
+<node CREATED="1447999595191" ID="ID_1083267780" MODIFIED="1447999600234" TEXT="Good mentors"/>
+<node CREATED="1447999602880" ID="ID_1672270016" MODIFIED="1447999607436" TEXT="Good trainers"/>
+<node CREATED="1447999610171" ID="ID_1293916137" MODIFIED="1447999616934" TEXT="Technical experts"/>
+<node CREATED="1447999618517" ID="ID_1328080617" MODIFIED="1447999622975" TEXT="Domain experts"/>
+<node CREATED="1447999875539" ID="ID_118663559" MODIFIED="1447999879877" TEXT="Leaders"/>
+</node>
+<node CREATED="1447999682761" ID="ID_1188373602" MODIFIED="1447999714743" TEXT="What are you doing ? Is it a right question ?"/>
+</node>
+</node>
+</map>
---
+++ Library/My Goals Resources.mm
@@ -0,0 +1,22 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1448346481649" ID="ID_315396437" MODIFIED="1448368663891" TEXT="Building Effective teams">
+<node CREATED="1448352239835" ID="ID_27041692" MODIFIED="1448352255205" POSITION="right" TEXT="The secret of teams"/>
+<node CREATED="1448352278701" ID="ID_1617398977" MODIFIED="1448352285951" POSITION="left" TEXT="A team of leaders"/>
+<node CREATED="1448352395432" ID="ID_1921747414" MODIFIED="1448352397067" POSITION="right" TEXT="The Heart of Leadership: Becoming a Leader People Want to Follow"/>
+<node CREATED="1448352437339" ID="ID_1597894232" MODIFIED="1448352440437" POSITION="left" TEXT="The 5 Levels of Leadership: Proven Steps to Maximize Your Potential "/>
+<node CREATED="1448352771678" ID="ID_788144127" MODIFIED="1448352772879" POSITION="right" TEXT="Lead Inside the Box: How Smart Leaders Guide Their Teams to Exceptional Results"/>
+<node CREATED="1448352783734" ID="ID_1610391942" MODIFIED="1448352784993" POSITION="left" TEXT="Leadership: Elevate Yourself and Those Around You - Influence, Business Skills, Coaching, &amp; Communication"/>
+<node CREATED="1448352818464" ID="ID_1881962222" MODIFIED="1448369139702" POSITION="right" TEXT="http://hrweb.berkeley.edu/guides/managing-hr/interaction/team-building/steps">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1448352832528" ID="ID_1941511676" MODIFIED="1448352833808" POSITION="left" TEXT="http://www.forbes.com/sites/theyec/2013/06/07/five-ways-to-build-an-effective-team/"/>
+<node CREATED="1448352855737" ID="ID_1402637768" MODIFIED="1448369143055" POSITION="right" TEXT="http://www.inc.com/peter-economy/5-steps-to-really-effective-teams.html">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1448360977964" ID="ID_1040720060" MODIFIED="1448360979579" POSITION="left" TEXT="Managers As Mentors: Building Partnerships for Learning "/>
+<node CREATED="1448360988163" ID="ID_1285964230" MODIFIED="1448360989300" POSITION="right" TEXT="Help Them Grow or Watch Them Go: Career Conversations Employees Want"/>
+<node CREATED="1448360997916" ID="ID_1873647424" MODIFIED="1448360999003" POSITION="left" TEXT="Coaching for Leadership: Writings on Leadership from the World&apos;s Greatest Coaches"/>
+<node CREATED="1448361008316" ID="ID_760686284" MODIFIED="1448361009458" POSITION="right" TEXT="Help Them Grow or Watch Them Go: Career Conversations Employees Want "/>
+</node>
+</map>
