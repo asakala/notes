21ed6bf0ed7fa7c36f39c1167b951c2a22f13014
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-11-25 09:27:35 -0600

    dfsdfs

---
+++ Prokarma/Effective Training.mm
@@ -0,0 +1,22 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1448445110120" ID="ID_1691483511" MODIFIED="1448445175743" TEXT="Effective Training">
+<node CREATED="1448445252283" ID="ID_723905339" MODIFIED="1448451600433" POSITION="left" TEXT="Issues with current training group">
+<node CREATED="1448445296904" ID="ID_12656523" MODIFIED="1448445544995" TEXT="No feedback on my session and there is no area of improvement"/>
+<node CREATED="1448445327543" ID="ID_733400303" MODIFIED="1448445381267" TEXT="I found lot of guys coming to me asking for PPT . They don&apos;t know where to find it"/>
+<node CREATED="1448445371987" ID="ID_169790856" MODIFIED="1448445421566" TEXT="Lot of employees ask me for presentation that I have made in the past and I am not sure where they are ?"/>
+<node CREATED="1448445432976" ID="ID_483017709" MODIFIED="1448445444846" TEXT="Quality of sessions are extremely poor"/>
+<node CREATED="1448445446510" ID="ID_1806497481" MODIFIED="1448451368930" TEXT="People lost interest in session - As a result there are no trainers or listeners"/>
+<node CREATED="1448445463214" ID="ID_1282442270" MODIFIED="1448445479663" TEXT="How to make a request for session"/>
+<node CREATED="1448451509028" ID="ID_1009422857" MODIFIED="1448452498665" TEXT="As a manger I don,t know how many trainings are given by my team members  and how many attended and how many are useful. I want everything in one place"/>
+<node CREATED="1448452420652" ID="ID_578155783" MODIFIED="1448452462958" TEXT="I want to do certification . Who has done certification so that I can use him as a mentor"/>
+</node>
+<node CREATED="1448451378823" ID="ID_1736498515" MODIFIED="1448451500787" POSITION="right" TEXT="Well defined process to address all these issues">
+<node CREATED="1448451859922" ID="ID_172968160" MODIFIED="1448451868172" TEXT="Three Objectives">
+<node CREATED="1448451922419" ID="ID_888530223" MODIFIED="1448452042985" TEXT="Empower existing employees , new employees , freshers"/>
+<node CREATED="1448451870834" ID="ID_1329151211" MODIFIED="1448452539116" TEXT="Attract good trainers and encourage then to train people continously"/>
+<node CREATED="1448451882522" ID="ID_1582057717" MODIFIED="1448451904159" TEXT="Systematically eliminate poor trainers"/>
+</node>
+</node>
+</node>
+</map>
