6cbb9b5549b86e99e918efd62691a3784878fb0c
Anil Sakala <anil.sakala@gmail.com>
2015-11-29 20:34:32 +0530

    dssdS

---
+++ Prokarma/LearningDepartment.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1448648252134" ID="ID_739391470" MODIFIED="1448710261246" TEXT="L &amp; D  Expectations">
+<node CREATED="1448648307170" ID="ID_1960923908" MODIFIED="1448710142628" POSITION="right" TEXT="Trainings is one of the pillars in Center Of Excellence"/>
+<node CREATED="1448648390985" ID="ID_907045962" MODIFIED="1448648394287" POSITION="left" TEXT="Freshers"/>
+<node CREATED="1448648433606" ID="ID_1989450920" MODIFIED="1448710141587" POSITION="right" TEXT="Fulfill project need / career needs"/>
+<node CREATED="1448648675162" ID="ID_1404661993" MODIFIED="1448648683976" POSITION="left" TEXT="Certifications"/>
+<node CREATED="1448648716146" ID="ID_1806904176" MODIFIED="1448710140498" POSITION="right" TEXT="Onboarding trainings"/>
+<node CREATED="1448648749398" ID="ID_1332339084" MODIFIED="1448648755850" POSITION="left" TEXT="Career Switch"/>
+<node CREATED="1448648773034" HGAP="21" ID="ID_1278510537" MODIFIED="1448710139786" POSITION="right" TEXT="Mandatory trainings that add value to teams and organization"/>
+<node CREATED="1448649402729" ID="ID_1261386113" MODIFIED="1448649421632" POSITION="left" TEXT="Empowering people in pool"/>
+</node>
+</map>
