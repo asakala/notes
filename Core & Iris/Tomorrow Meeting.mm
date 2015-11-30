bba7cb36917cdc5318d12089d7f022e1440b22d6
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-11-30 09:23:41 -0600

    sdsdfsd

---
+++ Core & Iris/Tomorrow Meeting.mm
@@ -0,0 +1,24 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447855185886" ID="ID_744215655" MODIFIED="1447855429078" TEXT="Tomorrow Meeting">
+<node CREATED="1447855239724" ID="ID_1795389991" MODIFIED="1447855244978" POSITION="right" TEXT="Good Things">
+<node CREATED="1447855249012" ID="ID_498401038" MODIFIED="1447855290273" TEXT="Good initiative by Lavanya and Team going by self driven approach "/>
+<node CREATED="1447855304415" ID="ID_656441366" MODIFIED="1447855410429" TEXT="Prod alerts - Lavanya , Ravi Teja &amp; Firoz"/>
+<node CREATED="1447855444571" ID="ID_414772305" MODIFIED="1447855636109" TEXT="Goutam has completed documentation part for TCS to netcontrol project - Lavanya need to validate"/>
+<node CREATED="1447856560296" ID="ID_1519536284" MODIFIED="1447856729142" TEXT="FS task is high priority one "/>
+<node CREATED="1447856730924" ID="ID_1543702352" MODIFIED="1447856780814" TEXT="PIT is a tool that need to be explored and happy to hear that Firoz has just installed it"/>
+<node CREATED="1447856851263" ID="ID_74473713" MODIFIED="1447856869534" TEXT="Good to see Gangadhar and Firoz work on weekend"/>
+</node>
+<node CREATED="1447855322295" ID="ID_55315979" MODIFIED="1447855676535" POSITION="left" TEXT="Areas of improvement">
+<node CREATED="1447855331137" ID="ID_751146988" MODIFIED="1447855352610" TEXT="Think Big we are thinking low"/>
+<node CREATED="1447855358952" ID="ID_1408013995" MODIFIED="1447855379947" TEXT="Opportunities for few employees"/>
+<node CREATED="1447855638658" ID="ID_1343972438" MODIFIED="1447855656725" TEXT="Viswanath to talk about how we can improve onboarding process"/>
+<node CREATED="1447855690708" ID="ID_1872054962" MODIFIED="1447855697246" TEXT="Focus on certification"/>
+<node CREATED="1447855880996" ID="ID_1774647217" MODIFIED="1447855904791" TEXT="What we can do so that we have better relationship with one another"/>
+<node CREATED="1447856087537" ID="ID_1163977386" MODIFIED="1447856103286" TEXT="Innovative ideas to promote prokarma brand"/>
+<node CREATED="1447856117106" ID="ID_1482640902" MODIFIED="1447856133749" TEXT="Improvement in the way we are doing interviews"/>
+<node CREATED="1447855676536" ID="ID_1940534219" MODIFIED="1447855689119" TEXT="I had sent a link on invest in yourself"/>
+<node CREATED="1447920780357" ID="ID_479970529" MODIFIED="1447920790251" TEXT="Jira link to track extra efforts"/>
+</node>
+</node>
+</map>
