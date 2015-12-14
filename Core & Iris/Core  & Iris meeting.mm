7d24bd60c2b87bf08df6af0975923e8e05ccc83b
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-12-14 03:11:04 -0600

    sdfdsf

--- Core & Iris/Core  & Iris meeting.mm
+++ Core & Iris/Core  & Iris meeting.mm
@@ -2,22 +2,16 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1447855185886" ID="ID_744215655" MODIFIED="1448346441656" TEXT="Tomorrow Meeting">
 <node CREATED="1447999418467" ID="ID_951425912" MODIFIED="1448896065709" POSITION="right" TEXT="Good Things ">
-<node CREATED="1447999507810" ID="ID_784329232" MODIFIED="1448028082897" TEXT="Excellent participation in code reviews and we should learn from code reviews"/>
-<node CREATED="1447999724510" ID="ID_517237908" MODIFIED="1447999739603" TEXT="Firoz and Goutam helped in assessing question paper"/>
-<node CREATED="1448004172518" ID="ID_939328844" MODIFIED="1448004176154" TEXT="Prod alerts">
-<node CREATED="1448004177610" ID="ID_1316655348" MODIFIED="1449419486430" TEXT="Viswanath Mitta , Appreciation mail"/>
+<node CREATED="1449730623462" ID="ID_1737010390" MODIFIED="1449730627910" TEXT="Production alerts">
+<node CREATED="1449730630078" ID="ID_1295492421" MODIFIED="1449753482597" TEXT="Lavanya - 3"/>
+<node CREATED="1449817959709" ID="ID_395518913" MODIFIED="1449817965424" TEXT="Goutam - 2"/>
 </node>
-<node CREATED="1448531431652" ID="ID_1502965335" MODIFIED="1448531443377" TEXT="Good to see firoz working on PIT"/>
-<node CREATED="1448532413746" ID="ID_492798726" MODIFIED="1448532428334" TEXT="Junit by giutam and Amar"/>
-<node CREATED="1448532442961" ID="ID_1359490248" MODIFIED="1448896123136" TEXT="Outing - Thanks to goutam , "/>
-<node CREATED="1448896067112" ID="ID_1093467330" MODIFIED="1448896093912" TEXT="Excellent participation in retro . Unbelievable growth in the way you look at teams"/>
-<node CREATED="1448896191713" ID="ID_1706168604" MODIFIED="1448896230979" TEXT="Firoz took initiative to take for one of the issues raised by Shawn. Good to hear this"/>
-<node CREATED="1448980269026" ID="ID_10720564" MODIFIED="1449419458908" TEXT="Good work by Lavanya in driving sprint plan . Who will take next month ?"/>
+<node CREATED="1449730637566" ID="ID_1472591412" MODIFIED="1449730660072" TEXT="TMX / Core / Iris - Jenkins failures">
+<node CREATED="1449730663558" ID="ID_577733757" MODIFIED="1449730670642" TEXT="Lavanya - 1"/>
+</node>
 </node>
 <node CREATED="1447999427597" ID="ID_610997063" MODIFIED="1447999440489" POSITION="left" TEXT="Topics for discussion">
-<node CREATED="1447999529577" ID="ID_203588763" MODIFIED="1448896401467" TEXT="Important people assets - mentors , trainers , leaders , technical and project management experts"/>
-<node CREATED="1447999682761" ID="ID_1188373602" MODIFIED="1447999714743" TEXT="What are you doing ? Is it a right question ?"/>
-<node CREATED="1448896135464" ID="ID_550725524" MODIFIED="1448896135464" TEXT=""/>
+<node CREATED="1449588908753" ID="ID_898627020" MODIFIED="1449588913948" TEXT="Survey questions"/>
 </node>
 </node>
 </map>
