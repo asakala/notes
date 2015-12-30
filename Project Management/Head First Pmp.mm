7475112112eb0a63fc0b5b453c4440ce18bfe082
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2015-12-30 08:41:28 -0600

    sfsdf

---
+++ Project Management/Head First Pmp.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1451388935864" ID="ID_916279433" MODIFIED="1451394747014" TEXT="Head First Pmp"/>
+</map>
---
+++ Project Management/Jira.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1451388965264" ID="ID_268098921" MODIFIED="1451389360541" TEXT="Jira">
+<node CREATED="1451389366008" ID="ID_1841901227" MODIFIED="1451393744042" POSITION="right" TEXT="Links">
+<node CREATED="1451389369697" ID="ID_192637575" MODIFIED="1451389371432" TEXT="https://confluence.atlassian.com/jirasoftwareserver070/getting-started-as-a-jira-software-manager-762877210.html"/>
+<node CREATED="1451392920251" ID="ID_164696646" MODIFIED="1451392922298" TEXT="https://mydemo.atlassian.net">
+<node CREATED="1451392930924" ID="ID_1670871144" MODIFIED="1451392939150" TEXT="anilprism@gmail.com"/>
+<node CREATED="1451392940468" ID="ID_1382953009" MODIFIED="1451392944426" TEXT="abc123india"/>
+</node>
+</node>
+</node>
+</map>
