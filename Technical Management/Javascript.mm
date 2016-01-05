dd5a38ad50d1ff41ad2952ac333328d0085a1aca
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-01-05 07:53:01 -0600

    sdfdsfsfs

---
+++ Technical Management/Javascript.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1451998810679" ID="ID_1855915226" MODIFIED="1451998899297" TEXT="Javascript"/>
+</map>
