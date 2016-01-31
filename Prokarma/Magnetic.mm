3a54f2cbbe81d445b40dc64eeed256d569eff587
Anil Sakala <anil.sakala@gmail.com>
2016-01-31 21:49:35 +0530

    dfdf

---
+++ Prokarma/Magnetic.mm
@@ -0,0 +1,26 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1454084683706" ID="ID_1713239785" MODIFIED="1454084724368" TEXT="Magnetic">
+<node CREATED="1454084845151" ID="ID_163494041" MODIFIED="1454084848201" POSITION="right" TEXT="Agenda">
+<node CREATED="1454089421584" HGAP="26" ID="ID_1157735816" MODIFIED="1454090144134" TEXT="Deep dive into engaging work environment with psychological , scientific and practical evidences leading to various action items" VSHIFT="-6"/>
+</node>
+<node CREATED="1454084981312" ID="ID_868159383" MODIFIED="1454084981312" POSITION="left" TEXT=""/>
+<node CREATED="1454084991620" ID="ID_1241706179" MODIFIED="1454084998257" POSITION="right" TEXT="What was discussed ?"/>
+<node CREATED="1454085000283" ID="ID_710570964" MODIFIED="1454085000283" POSITION="left" TEXT=""/>
+<node CREATED="1454085003547" ID="ID_779698462" MODIFIED="1454085014710" POSITION="right" TEXT="What is not discussed ?">
+<node CREATED="1454085044980" ID="ID_621193011" MODIFIED="1454085099860" TEXT="How can we assess engagement levels of employees in a company"/>
+<node CREATED="1454085116534" ID="ID_459801913" MODIFIED="1454086976348" TEXT="Deep dive into how engagement improves productivity ? Is this the only way to improve productivity ?">
+<node CREATED="1454141029224" ID="ID_184731469" MODIFIED="1454141031807" TEXT="done"/>
+</node>
+<node CREATED="1454085139035" ID="ID_174029101" MODIFIED="1454086993004" TEXT="Deep dive into how engagement improves quality of work ? Is this the only way to improve quality ?"/>
+<node CREATED="1454087514475" ID="ID_1467255978" MODIFIED="1454087578474" TEXT="Deep dive into how engaging work environment improves customer satisfaction ? "/>
+<node CREATED="1454085193244" ID="ID_537343325" MODIFIED="1454092590491" TEXT="Why pay  is not included in engagement factors ?"/>
+<node CREATED="1454085208921" ID="ID_684358175" MODIFIED="1454092514356" TEXT="Why teams outings , team lunches ,  free food , gifts , rangoli are not included in engagement factors ?"/>
+<node CREATED="1454088100668" ID="ID_70020697" MODIFIED="1454092557187" TEXT="How to handle factors that create disengaging work environment - Project failures , Rumors "/>
+<node CREATED="1454088797383" ID="ID_1400630483" MODIFIED="1454089261526" TEXT="Why engagement starts with right hiring and right onboarding ?">
+<node CREATED="1454175010903" ID="ID_1345286256" MODIFIED="1454175010903" TEXT=""/>
+</node>
+<node CREATED="1454085286490" ID="ID_1010383975" MODIFIED="1454085609900" TEXT="Action items that we need to work on  to improve overall engagement levels ?"/>
+</node>
+</node>
+</map>
