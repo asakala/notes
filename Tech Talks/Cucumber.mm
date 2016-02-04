91c4263b1b0e70416328dff7edc57aa36a4adfbf
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-02-04 03:05:37 -0600

    sdfsdf

--- Tech Talks/Cucumber.mm
+++ Tech Talks/Cucumber.mm
@@ -1,4 +1,34 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1454058237049" ID="ID_999927508" MODIFIED="1454059664101" TEXT="Cucumber"/>
+<node CREATED="1454058237049" ID="ID_999927508" MODIFIED="1454059664101" TEXT="Cucumber">
+<node CREATED="1454425456813" ID="ID_831784144" MODIFIED="1454501519604" POSITION="right" TEXT="Chapter - 1">
+<node CREATED="1454425481800" HGAP="23" ID="ID_1092175397" MODIFIED="1454498256723" TEXT="Big Problem :  The biggest problem that we are facing today in IT industry is communication gap between domain experts and technical people" VSHIFT="-1">
+<node CREATED="1454425557722" ID="ID_1508785994" MODIFIED="1454425631023" TEXT="In conventional practice domain experts write some document and hand it over to developers which is not complete to translate to code well "/>
+<node CREATED="1454426315615" ID="ID_110099974" MODIFIED="1454426561749" TEXT="We should establish a way that reduces this communication gap between developers and BA&apos;s"/>
+<node CREATED="1454426376801" ID="ID_675350325" MODIFIED="1454426415697" TEXT="This thinking led to a way of development called BDD (Behaviour Driven Development)"/>
+</node>
+<node CREATED="1454427498448" HGAP="64" ID="ID_1659810731" MODIFIED="1454497954687" TEXT="Behaviour driven development" VSHIFT="60">
+<node CREATED="1454488807666" ID="ID_320548112" MODIFIED="1454488818080" TEXT="Every application has fetaures"/>
+<node CREATED="1454488820345" ID="ID_1164694292" MODIFIED="1454488836371" TEXT="For every we define certain behaviour"/>
+<node CREATED="1454488852554" ID="ID_404473132" MODIFIED="1454488854315" TEXT="All coding efforts are geared toward delivering these desired behaviors."/>
+<node CREATED="1454488927676" ID="ID_1450251379" MODIFIED="1454488942767" TEXT="These behaviours form acceptance tests"/>
+<node CREATED="1454489071610" ID="ID_353594812" MODIFIED="1454489141738" TEXT="Common language that can be understood by both systems and users need to be developed for writing these behaviours"/>
+<node CREATED="1454489192014" ID="ID_281003787" MODIFIED="1454489247449" TEXT="One such tool that helps you in implementing BDD is cucumber"/>
+</node>
+<node CREATED="1454489415431" HGAP="21" ID="ID_1544757292" MODIFIED="1454490454210" TEXT="Cucumber" VSHIFT="29">
+<node CREATED="1454489784200" ID="ID_910284517" MODIFIED="1454489833448" TEXT="If you want to develop project using cucumber first we need to develop feature files"/>
+<node CREATED="1454489845347" ID="ID_120472899" MODIFIED="1454489883366" TEXT="These feature files are developed using language called GHERKIN"/>
+<node CREATED="1454490075091" ID="ID_747859133" MODIFIED="1454490110387" TEXT="Every feature file will have number of scenarios . Each scenario is acceptance test"/>
+<node CREATED="1454490133309" ID="ID_1036473219" MODIFIED="1454490143506" TEXT="Scenario contains several steps"/>
+<node CREATED="1454490305515" ID="ID_379378394" MODIFIED="1454490349000" TEXT="All above steps are completed by domain experts"/>
+<node CREATED="1454490334429" ID="ID_1125750757" MODIFIED="1454490385618" TEXT="Developers will write code to validate these files against system"/>
+<node CREATED="1454490387358" ID="ID_1305920630" MODIFIED="1454490425508" TEXT="Developers will be writing step definitions , support code and automation library"/>
+</node>
+<node CREATED="1454490462449" ID="ID_525104126" MODIFIED="1454490468325" TEXT="Conclusion">
+<node CREATED="1454490469770" ID="ID_1343630597" MODIFIED="1454490492399" TEXT="BDD is a concrete way of capturing requirements "/>
+<node CREATED="1454490495586" ID="ID_1929886035" MODIFIED="1454498226127" TEXT="In BDD we create a document that is used by both BA and acts as automation test suite by the system"/>
+</node>
+<node CREATED="1454501520886" HGAP="27" ID="ID_1239365051" LINK="FeatureFileEx1.jpg" MODIFIED="1454502556747" TEXT="Example feature file" VSHIFT="60"/>
+</node>
+</node>
 </map>
