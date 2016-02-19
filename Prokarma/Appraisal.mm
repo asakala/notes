412e4d7f34d01508ea6975efdca818d6337c8c08
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-02-19 10:14:34 -0600

    dsfs

---
+++ Prokarma/Appraisal.mm
@@ -0,0 +1,37 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1455777699287" ID="ID_670812255" MODIFIED="1455780474822" TEXT="Appraisal">
+<node CREATED="1455777719299" HGAP="5" ID="ID_1168925908" MODIFIED="1455801199333" POSITION="right" TEXT="Myths &amp; Realities about appraisal system" VSHIFT="-102">
+<node CREATED="1455778215678" HGAP="17" ID="ID_1458569930" MODIFIED="1455797620359" TEXT="Performance appraisal improves performance" VSHIFT="-9">
+<node CREATED="1455781919305" ID="ID_1874832410" MODIFIED="1455781921361" TEXT="https://www.psychologytoday.com/blog/wired-success/201402/why-performance-appraisals-dont-improve-performance"/>
+<node CREATED="1455783053226" ID="ID_654988491" MODIFIED="1455783055222" TEXT="http://business.financialpost.com/executive/careers/why-performance-appraisals-need-to-be-scrapped"/>
+</node>
+<node CREATED="1455778217622" HGAP="33" ID="ID_1473483029" MODIFIED="1455801059217" TEXT="Conclusion" VSHIFT="19">
+<node CREATED="1455778433567" ID="ID_516850703" MODIFIED="1455778458705" TEXT="Zero significance to business results">
+<node CREATED="1455778675838" ID="ID_387611086" MODIFIED="1455778695521" TEXT="Solution : Feedback cycles"/>
+</node>
+<node CREATED="1455778461433" ID="ID_380920583" MODIFIED="1455778491320" TEXT="Associated with serious emotional pain">
+<node CREATED="1455778697399" ID="ID_1260431997" MODIFIED="1455778717201" TEXT="Solution : Reduce to four point scale"/>
+</node>
+<node CREATED="1455778498912" ID="ID_784223099" MODIFIED="1455778537778" TEXT="Performance based salary hikes are important">
+<node CREATED="1455778735558" ID="ID_1266986961" MODIFIED="1455778762160" TEXT="Solution : Four point scale"/>
+</node>
+</node>
+<node CREATED="1455789131481" HGAP="18" ID="ID_1182886605" MODIFIED="1455797623295" TEXT="What kind of performers do you see in work environment" VSHIFT="38">
+<node CREATED="1455778216655" ID="ID_1634165517" MODIFIED="1455798915278" TEXT="Performance follows bell curve so let us use 5 point scale" VSHIFT="-11">
+<node CREATED="1455781289779" HGAP="26" ID="ID_1067568798" MODIFIED="1455799942615" TEXT="http://www.forbes.com/sites/joshbersin/2014/02/19/the-myth-of-the-bell-curve-look-for-the-hyper-performers/#681dbe8513fc" VSHIFT="-7"/>
+</node>
+<node CREATED="1455796723406" ID="ID_1306141364" MODIFIED="1455796751820" TEXT="Conclusion : Your grading system should reflect these statics"/>
+</node>
+</node>
+<node CREATED="1455780475702" ID="ID_1494145154" MODIFIED="1455780479509" POSITION="left" TEXT="References">
+<node CREATED="1455780480718" ID="ID_1230174710" MODIFIED="1455780482394" TEXT="https://www.washingtonpost.com/news/on-leadership/wp/2015/07/21/in-big-move-accenture-will-get-rid-of-annual-performance-reviews-and-rankings/"/>
+<node CREATED="1455780494999" ID="ID_1779495887" MODIFIED="1455780496277" TEXT="https://www.youtube.com/watch?v=yXNh1xBdmFQ"/>
+<node CREATED="1455780506648" ID="ID_710178129" MODIFIED="1455780507726" TEXT="http://economictimes.indiatimes.com/tech/ites/need-to-reinvent-and-transform-in-todays-age-virginia-rometty-ibm/articleshow/50830431.cms"/>
+<node CREATED="1455780927870" ID="ID_1304850457" MODIFIED="1455780929830" TEXT="http://www.forbes.com/sites/joshbersin/2014/02/19/the-myth-of-the-bell-curve-look-for-the-hyper-performers/#681dbe8513fc"/>
+<node CREATED="1455783507278" ID="ID_1183565561" MODIFIED="1455783509380" TEXT="http://www.amazon.com/Management-Practices-Waste-Money-instead/dp/093710017X/ref=tmm_hrd_swatch_0?_encoding=UTF8&amp;qid=&amp;sr="/>
+<node CREATED="1455797692361" ID="ID_1563747987" MODIFIED="1455797694114" TEXT="http://business.financialpost.com/executive/careers/why-performance-appraisals-need-to-be-scrapped"/>
+<node CREATED="1455798181642" ID="ID_216343695" LINK="The%20Best%20Are%20Much%20Better%20Than%20the%20Rest.docx" MODIFIED="1455798215823" TEXT="doc"/>
+</node>
+</node>
+</map>
