381ff92736943f7f6211d88b6b73d4efdb6fc3cb
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-03-23 09:35:25 -0500

    sdfsdfs

--- Technical Management/Comments.mm
+++ Technical Management/Comments.mm
@@ -10,8 +10,7 @@
       Comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458566043869" HGAP="12" ID="ID_1999406966" MODIFIED="1458637767146" POSITION="right" STYLE="fork" VSHIFT="-70">
 <richcontent TYPE="NODE"><html>
@@ -23,8 +22,7 @@
       Bad comments&#160;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458566583065" ID="ID_496854943" MODIFIED="1458637767147" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -36,10 +34,9 @@
       Never write a comment if you can replace with expressive code
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1458636842370" ID="ID_462139262" MODIFIED="1458637767148" STYLE="fork">
+<node CREATED="1458636842370" ID="ID_462139262" MODIFIED="1458736604998" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -49,8 +46,7 @@
       AonCreateServiceRequestHandler.java
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458636865723" ID="ID_1566280588" MODIFIED="1458637767149" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -62,8 +58,7 @@
       142
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -78,8 +73,7 @@
       Comments that are in complete and doesn't give full details
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458567010928" ID="ID_1728248344" MODIFIED="1458637767150" STYLE="fork">
@@ -92,9 +86,13 @@
       Redundant comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458723352411" ID="ID_547340506" MODIFIED="1458723354905" TEXT="SendMilitary">
+<node CREATED="1458723356522" ID="ID_994109241" MODIFIED="1458723359215" TEXT="154"/>
+<node CREATED="1458723380172" ID="ID_707700126" MODIFIED="1458723384035" TEXT="72"/>
+<node CREATED="1458723404740" ID="ID_916239789" MODIFIED="1458723407905" TEXT="83"/>
+</node>
 </node>
 <node CREATED="1458567027497" ID="ID_1645746323" MODIFIED="1458637767151" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -106,9 +104,11 @@
       Misleading comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458723114729" ID="ID_521105747" MODIFIED="1458723126368" TEXT="SendMilitary">
+<node CREATED="1458723128344" ID="ID_1830277229" MODIFIED="1458723130617" TEXT="159"/>
+</node>
 </node>
 <node CREATED="1458569320528" ID="ID_482777568" MODIFIED="1458637767152" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -120,11 +120,11 @@
       Mandated comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458722852393" ID="ID_1481418487" MODIFIED="1458722854334" TEXT="AonQueryServiceRequestHandler"/>
 </node>
-<node CREATED="1458569329950" ID="ID_807367934" MODIFIED="1458637767153" STYLE="fork">
+<node CREATED="1458569329950" ID="ID_807367934" MODIFIED="1458735440243" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -134,10 +134,9 @@
       Journal comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1458637165413" ID="ID_273484724" MODIFIED="1458637767153" STYLE="fork">
+<node CREATED="1458637165413" ID="ID_273484724" MODIFIED="1458737160778" STYLE="fork">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -147,8 +146,7 @@
       HSCReceiveExamResultsService
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637216807" ID="ID_944463733" MODIFIED="1458637767154" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -160,8 +158,7 @@
       2045
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -178,8 +175,7 @@
       Noise comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637538642" ID="ID_1079397241" MODIFIED="1458637767156" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -191,8 +187,7 @@
       ConfirmMessagePanel
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637544130" ID="ID_1445575279" MODIFIED="1458637767156" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -204,8 +199,7 @@
       32
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -219,8 +213,7 @@
       HraCand
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637692799" ID="ID_1211149197" MODIFIED="1458637767158" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -232,8 +225,7 @@
       105
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -248,9 +240,11 @@
       Scary noise
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458723770409" ID="ID_173215336" MODIFIED="1458723779023" TEXT="SendMilitary">
+<node CREATED="1458723782138" ID="ID_162098972" MODIFIED="1458723785211" TEXT="469"/>
+</node>
 </node>
 <node CREATED="1458569396994" ID="ID_1197832349" MODIFIED="1458637767159" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -262,8 +256,7 @@
       Donot use a comment when you can use a function or a variable&#160;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458569430968" ID="ID_830244108" MODIFIED="1458637767160" STYLE="fork">
@@ -276,8 +269,7 @@
       Position markers
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458569453359" ID="ID_956407223" MODIFIED="1458637767160" STYLE="fork">
@@ -290,9 +282,11 @@
       Closing brace comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458722984457" ID="ID_508350683" MODIFIED="1458722987114" TEXT="SendMilitary">
+<node CREATED="1458722992033" ID="ID_565502506" MODIFIED="1458722993985" TEXT="182"/>
+</node>
 </node>
 <node CREATED="1458569466686" ID="ID_1452709497" MODIFIED="1458637767162" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -304,19 +298,13 @@
       Commented out code
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
-<node CREATED="1458570065630" ID="ID_1435369467" MODIFIED="1458637767163" STYLE="fork">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-  </body>
-</html>
-</richcontent>
-<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458722773832" ID="ID_805869590" MODIFIED="1458722776572" TEXT="DecodeMgmt">
+<node CREATED="1458722783750" ID="ID_313143800" MODIFIED="1458722786471" TEXT="104"/>
+</node>
+<node CREATED="1458724142599" ID="ID_1214138068" MODIFIED="1458724144046" TEXT="ReverseExpiringService">
+<node CREATED="1458724147134" ID="ID_1897891128" MODIFIED="1458724149102" TEXT="210"/>
 </node>
 </node>
 <node CREATED="1458569491540" ID="ID_770761818" MODIFIED="1458637767164" STYLE="fork">
@@ -329,8 +317,7 @@
       HTML comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637036897" ID="ID_917502002" MODIFIED="1458637767164" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -342,8 +329,7 @@
       AonQueryServiceRequestHandler
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -357,8 +343,7 @@
       Too much information
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458569550419" ID="ID_466976620" MODIFIED="1458637767166" STYLE="fork">
@@ -371,8 +356,7 @@
       Javadocs in non public code
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458637089866" ID="ID_1049949096" MODIFIED="1458637767166" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -384,8 +368,7 @@
       AonServiceResponseHandler
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
@@ -400,8 +383,7 @@
       Good Comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458566613178" ID="ID_209418828" MODIFIED="1458637767171" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -413,8 +395,7 @@
       Informative comments - Write a comment if it is not possible to replace with expressive code and comment is helpful in increasing readability of code
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458566745655" ID="ID_189530305" MODIFIED="1458637767172" STYLE="fork">
@@ -427,8 +408,7 @@
       TODO comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
 </node>
 <node CREATED="1458566801794" ID="ID_478363895" MODIFIED="1458637767173" STYLE="fork">
@@ -441,9 +421,10 @@
       Java doc in public API's
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458725898548" ID="ID_1445224012" MODIFIED="1458725915387" TEXT="Shared utils need java documentation"/>
+<node CREATED="1458725986340" ID="ID_1355926807" MODIFIED="1458725987987" TEXT="https://docs.oracle.com/javase/7/docs/api/java/util/HashMap.html"/>
 </node>
 <node CREATED="1458566823890" ID="ID_39447459" MODIFIED="1458637767174" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -455,9 +436,9 @@
       Warning of consequences
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458734448624" ID="ID_1163690920" MODIFIED="1458734456802" TEXT="http://techbus.safaribooksonline.com/book/software-engineering-and-development/agile-development/9780136083238/chapter-4-comments/ch4lev2sec3#X2ludGVybmFsX0h0bWxWaWV3P3htbGlkPTk3ODAxMzYwODMyMzglMkZjaDRsZXYyc2VjNSZxdWVyeT0="/>
 </node>
 <node CREATED="1458566855242" ID="ID_1382223068" MODIFIED="1458637767174" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -469,9 +450,9 @@
       Explanation of intent
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458734397192" ID="ID_1949004210" MODIFIED="1458734399280" TEXT="http://techbus.safaribooksonline.com/book/software-engineering-and-development/agile-development/9780136083238/chapter-4-comments/ch4lev2sec3"/>
 </node>
 <node CREATED="1458566898109" ID="ID_679020939" MODIFIED="1458637767175" STYLE="fork">
 <richcontent TYPE="NODE"><html>
@@ -483,9 +464,9 @@
       Legal comments
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 <font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458725821488" ID="ID_1192922079" MODIFIED="1458725823054" TEXT="http://grepcode.com/file/repo1.maven.org/maven2/log4j/log4j/1.2.16/org/apache/log4j/lf5/viewer/configure/ConfigurationManager.java#ConfigurationManager"/>
 </node>
 </node>
 </node>
--- Technical Management/User Interface/Javascript/The principles of object oriented javascript.mm
+++ Technical Management/User Interface/Javascript/The principles of object oriented javascript.mm
@@ -2,12 +2,12 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1458560621713" ID="ID_1880227655" MODIFIED="1458647473304" TEXT="The principles of object oriented javascript">
 <node CREATED="1458647479536" ID="ID_419969028" MODIFIED="1458647489207" POSITION="right" TEXT="Primitive &amp; Reference types">
-<node CREATED="1458647681384" FOLDED="true" ID="ID_1998849043" MODIFIED="1458650506006" TEXT="Transition from traditional OOP">
+<node CREATED="1458647681384" ID="ID_1998849043" MODIFIED="1458742761805" TEXT="Transition from traditional OOP">
 <node CREATED="1458647883798" ID="ID_1960540743" MODIFIED="1458647965306" TEXT="Javascript doen&apos;t have concept called classes and packages"/>
 <node CREATED="1458647966625" ID="ID_1780190126" MODIFIED="1458648055319" TEXT="Javascript makes object central part of langugage. Even functions are treated as objects in javascript"/>
 <node CREATED="1458648136287" ID="ID_193347259" MODIFIED="1458648157666" TEXT="You can create objects at any time , add and remove properties from it"/>
 </node>
-<node CREATED="1458649715177" FOLDED="true" ID="ID_1680783937" MODIFIED="1458653901770" TEXT="Primitive Types">
+<node CREATED="1458649715177" ID="ID_1680783937" MODIFIED="1458743099036" TEXT="Primitive Types">
 <node CREATED="1458649970609" FOLDED="true" ID="ID_95879952" MODIFIED="1458650272468" TEXT="5 Types">
 <node CREATED="1458650089653" ID="ID_424359639" MODIFIED="1458650092574" TEXT="boolean"/>
 <node CREATED="1458650094045" ID="ID_1707260034" MODIFIED="1458650096281" TEXT="number"/>
@@ -25,7 +25,19 @@
 <node CREATED="1458654739111" ID="ID_1180373305" MODIFIED="1458654753498" TEXT="Objects can be visualized as hashmaps in javascript"/>
 <node CREATED="1458654756000" ID="ID_1174878372" MODIFIED="1458654875121" TEXT="Value can be primitve type  / object  (function , array , regular expression ,Date , Error  )"/>
 </node>
-<node CREATED="1458654722246" ID="ID_667068062" MODIFIED="1458654727145" TEXT="Creating objects"/>
+<node CREATED="1458654722246" ID="ID_667068062" MODIFIED="1458654727145" TEXT="Creating objects">
+<node CREATED="1458743337853" ID="ID_1869749018" MODIFIED="1458743349201" TEXT="var object = new Object();"/>
+<node CREATED="1458743359094" ID="ID_657737955" MODIFIED="1458743370012" TEXT="object.name = &quot;anil&quot;;"/>
+<node CREATED="1458743407096" ID="ID_915358831" MODIFIED="1458743420570" TEXT="You can add properties dynamically"/>
+</node>
+<node CREATED="1458743385911" ID="ID_1966263876" MODIFIED="1458743399666" TEXT="Creating objects for few built in types">
+<node CREATED="1458743452802" ID="ID_1747361499" MODIFIED="1458743456268" TEXT="Array"/>
+<node CREATED="1458743476650" ID="ID_562730809" MODIFIED="1458743478812" TEXT="Date"/>
+<node CREATED="1458743479794" ID="ID_1120765916" MODIFIED="1458743482054" TEXT="Error"/>
+<node CREATED="1458743485451" ID="ID_1483060244" MODIFIED="1458743488628" TEXT="Function"/>
+<node CREATED="1458743489346" ID="ID_1054730636" MODIFIED="1458743492055" TEXT="Object"/>
+<node CREATED="1458743494339" ID="ID_449090688" MODIFIED="1458743498203" TEXT="RegExp"/>
+</node>
 </node>
 </node>
 </node>
