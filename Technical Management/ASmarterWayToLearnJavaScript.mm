01fe6797a4a7ce53d405693072b123aaeb749e5c
Anil Sakala <anil.sakala@gmail.com>
2016-03-26 21:15:25 +0530

    dsfsf

---
+++ Technical Management/ASmarterWayToLearnJavaScript.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1457240765664" ID="ID_1507030213" MODIFIED="1457240765664" TEXT="New Mindmap"/>
+</map>
