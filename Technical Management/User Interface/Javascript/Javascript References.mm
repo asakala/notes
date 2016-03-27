079f5b487f77463daca2d91d06cc91032aa49bcc
Anil Sakala <Anil Sakala>
2016-03-27 08:14:27 +0530

    dfsd

---
+++ Technical Management/User Interface/Javascript/Javascript References.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1458560621713" ID="ID_1880227655" MODIFIED="1459016453273" TEXT="Javascript - References">
+<node CREATED="1459018013587" ID="ID_538631941" MODIFIED="1459018015433" POSITION="right" TEXT="https://www.youtube.com/watch?v=PMfcsYzj-9M"/>
+</node>
+</map>
