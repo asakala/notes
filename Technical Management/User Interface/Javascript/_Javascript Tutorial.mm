9fd8cab180757aa59f4972c672b8c875fd4f4c73
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-04-01 08:59:46 -0500

    dfsdfs

---
+++ Technical Management/User Interface/Javascript/_Javascript Tutorial.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1458560621713" ID="ID_1880227655" MODIFIED="1459490333493" TEXT="Javascript - Tutorial">
+<node CREATED="1459488754465" ID="ID_1657874385" MODIFIED="1459488769355" POSITION="right" TEXT="History of Javascript">
+<node CREATED="1459488883168" ID="ID_1552641824" MODIFIED="1459490044502" TEXT="Created by Brendan Eich"/>
+<node CREATED="1459489972653" ID="ID_858071053" MODIFIED="1459489996146" TEXT="Name of javascript standard - ECMAScript"/>
+<node CREATED="1459489997786" ID="ID_1260634994" MODIFIED="1459490022708" TEXT="17 year old become hot again because writ once and run anywhere on frontend"/>
+</node>
+</node>
+</map>
