d285f603bf93582bca608f58f30e17004fe9c83a
unknown <xprk828@PKMI-DS057E920.upcorp.ad.uprr.com>
2016-04-07 04:06:33 -0500

    fsdfs

---
+++ Technical Management/Enterprise Java/Core Java.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1449977351807" ID="ID_1961721350" MODIFIED="1460019825787" TEXT="Core Java">
+<node CREATED="1460019831302" ID="ID_1486914582" MODIFIED="1460019845972" POSITION="right" TEXT="What is java">
+<node CREATED="1460019847684" ID="ID_231706850" MODIFIED="1460019862692" TEXT="Why should I learn java"/>
+</node>
+</node>
+</map>
